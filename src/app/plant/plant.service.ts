91c383f648f2070333488512c22db069f041dc2e
Andres Soler <andres.soler@talent.com>
2022-11-03 13:11:30 -0500

    punto No. 1

---
+++ src/app/plant/plant.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Plant } from './plant';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantService {
+  private plantsUrl = environment.plantsUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getPlants(): Observable<Plant[]> {
+    return this.http.get<Plant[]>(this.plantsUrl);
+  }
+
+}
