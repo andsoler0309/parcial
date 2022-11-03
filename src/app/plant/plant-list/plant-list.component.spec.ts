0b908c6c3913a52cf0411fb053aef6c93a96c36f
Andres Soler <andres.soler@talent.com>
2022-11-03 13:29:17 -0500

    punto No.3

--- src/app/plant/plant-list/plant-list.component.spec.ts
+++ src/app/plant/plant-list/plant-list.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PlantListComponent } from './plant-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantService } from '../plant.service';
+import { Plant } from '../plant';
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
   let fixture: ComponentFixture<PlantListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ PlantListComponent ],
+      providers: [ PlantService ]
     })
     .compileComponents();
   }));
@@ -19,10 +26,44 @@ describe('PlantListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const plant = new Plant(
+        faker.datatype.number(),
+        faker.random.word(),
+        faker.random.word(),
+        faker.random.word(),
+        faker.datatype.number(),
+        faker.random.word(),
+        faker.lorem.sentence(),
+      );
+      component.plants.push(plant);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 plants', () => {
+    expect(component.plants.length).toBe(3);
+  });
+
+  it('should table have 3 rows', () => {
+    const rows = debug.queryAll(By.css('tbody tr'));
+    expect(rows.length).toBe(3);
+  });
+
+  it('should table have 4 total columns', () => {
+    const columns = debug.queryAll(By.css('thead th'));
+    expect(columns.length).toBe(4);
+  });
+
+  it('should table have total 4 rows', () => {
+    const rows = debug.queryAll(By.css('tbody tr'));
+    expect(rows.length).toBe(3);
   });
 });
--- src/app/plant/plant.service.spec.ts
+++ src/app/plant/plant.service.spec.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-unused-variable */
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlantService } from './plant.service';
 
 describe('Service: Plant', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [PlantService]
     });
   });
